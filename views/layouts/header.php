673ff76430652a352b6491545982c78e3dcb64b8
jojo6550 <josiah.johnson6550@gmail>
2026-03-30 10:41:54 -0500

    `Removed deprecated header and sidebar layouts`

--- views/layouts/header.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Deprecated: Use views/layouts/admin-start.php instead.
- * Kept as stub for backward compatibility.
- */
-?>
--- views/layouts/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Deprecated: Sidebar is now embedded in views/layouts/admin-start.php.
- * Kept as stub for backward compatibility.
- */
-?>
